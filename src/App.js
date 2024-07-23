f55e39057ce2c1257ddc2677219f439ff37db5dc
Akhila <akhiladharavath69@gmail.com>
2024-07-23 22:29:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Logos from './Logos';
+
+import image1 from "./Images/image1.png";
+import image2 from "./Images/image2.png";
+import image3 from "./Images/image3.png";
+import image4 from "./Images/image4.png";
+import image5 from "./Images/image5.png";
+import image6 from "./Images/image6.png";
+import image7 from "./Images/image7.png";
+
+const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+
+].map((image) => ({
+  id: crypto.randomUUID(),
+  image
+}))
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logos  images = {images} speed = {2000} />
     </div>
   );
 }
